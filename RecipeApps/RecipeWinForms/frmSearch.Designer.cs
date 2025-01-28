faaa5ea1626e53b175ec344ca8ce75f346d29381
Tzipporahuhr <tzipporahuhr@gmail.com>
2025-01-28 15:20:18 -0500

    trial

---
+++ RecipeApps/RecipeWinForms/frmSearch.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RecipeWinForms
+{
+    partial class frmSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tblMain = new TableLayoutPanel();
+            tblOptions = new TableLayoutPanel();
+            txtSearch = new TextBox();
+            btnSearch = new Button();
+            gRecipe = new DataGridView();
+            tblMain.SuspendLayout();
+            tblOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)gRecipe).BeginInit();
+            SuspendLayout();
+            // 
+            // tblMain
+            // 
+            tblMain.ColumnCount = 1;
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tblMain.Controls.Add(tblOptions, 0, 0);
+            tblMain.Controls.Add(gRecipe, 0, 1);
+            tblMain.Dock = DockStyle.Fill;
+            tblMain.Location = new Point(0, 0);
+            tblMain.Name = "tblMain";
+            tblMain.RowCount = 2;
+            tblMain.RowStyles.Add(new RowStyle());
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tblMain.Size = new Size(709, 529);
+            tblMain.TabIndex = 0;
+            // 
+            // tblOptions
+            // 
+            tblOptions.AutoSize = true;
+            tblOptions.ColumnCount = 2;
+            tblOptions.ColumnStyles.Add(new ColumnStyle());
+            tblOptions.ColumnStyles.Add(new ColumnStyle());
+            tblOptions.Controls.Add(txtSearch, 0, 0);
+            tblOptions.Controls.Add(btnSearch, 1, 0);
+            tblOptions.Location = new Point(3, 3);
+            tblOptions.Name = "tblOptions";
+            tblOptions.RowCount = 1;
+            tblOptions.RowStyles.Add(new RowStyle());
+            tblOptions.Size = new Size(231, 48);
+            tblOptions.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Dock = DockStyle.Fill;
+            txtSearch.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSearch.Location = new Point(3, 3);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(91, 39);
+            txtSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            btnSearch.AutoSize = true;
+            btnSearch.Location = new Point(100, 3);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(128, 42);
+            btnSearch.TabIndex = 1;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // gRecipe
+            // 
+            gRecipe.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gRecipe.Dock = DockStyle.Fill;
+            gRecipe.Location = new Point(3, 57);
+            gRecipe.Name = "gRecipe";
+            gRecipe.Size = new Size(703, 469);
+            gRecipe.TabIndex = 1;
+            // 
+            // frmSearch
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(709, 529);
+            Controls.Add(tblMain);
+            Name = "frmSearch";
+            Text = "Recipe Search";
+            tblMain.ResumeLayout(false);
+            tblMain.PerformLayout();
+            tblOptions.ResumeLayout(false);
+            tblOptions.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)gRecipe).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tblMain;
+        private TableLayoutPanel tblOptions;
+        private TextBox txtSearch;
+        private Button btnSearch;
+        private DataGridView gRecipe;
+    }
+}
---
+++ RecipeApps/RecipeWinForms/frmSearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecipeWinForms
+{
+    public partial class frmSearch : Form
+    {
+        public frmSearch()
+        {
+            InitializeComponent();
+
+            btnSearch.Click += BtnSearch_Click;
+        }
+
+        private void BtnSearch_Click(object? sender, EventArgs e)
+        { 
+        }
+
+        private void SearchForRecipe(string recipetype )
+        {
+            string sql = "select * from recipe r where";
+        }
+
+        private string GetConnectionString( )
+        {
+            var s = "Server=.\\SQLExpress;Database=RecordKeeperDB;Trusted_Connection=true";
+           
+            return s;
+        }
+
+        private DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = GetConnectionString();
+            conn.Open();
+            
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement; ;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+
+
+    }
+}
