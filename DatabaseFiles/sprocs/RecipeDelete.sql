ca5958f9d52662bfe0709de62041369775edcfa3
Tzipporahuhr <tzipporahuhr@gmail.com>
2025-06-23 17:02:02 -0400

    changes 21

--- DatabaseFiles/sprocs/RecipeDelete.sql
+++ DatabaseFiles/sprocs/RecipeDelete.sql
@@ -6,16 +6,6 @@ as
        begin try
 	   begin tran
 	
-	if
-		exists(select 1 from CourseMealRecipe where RecipeId=@RecipeId)
-	    or exists (select 1 from RecipeCookBook where @RecipeId= @RecipeId)
-	begin
-
-	rollback;
-	throw 500000, '',1;
-
-	end
-
 	delete from RecipeIngredient where RecipeId = @RecipeId;
 	delete from Direction where RecipeId = @RecipeId;
     delete from Recipe where RecipeId = @RecipeId;
